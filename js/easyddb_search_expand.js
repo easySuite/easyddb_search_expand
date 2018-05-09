d2293bc5f31588d5dfa9d4a4b257fc43460713e7
Nicolae Grigoriu <ng@inlead.dk>
2018-05-09 22:28:03 +0300

    HOTFIX - Toggle overlay class on search expand.

--- js/easyddb_search_expand.js
+++ js/easyddb_search_expand.js
@@ -7,12 +7,20 @@
   Drupal.behaviors.searchExpand = {
     attach: function (context) {
       $(function () {
-        var searchExpandBtn = $(context).find('.search-extended-button');
-        if (searchExpandBtn.length > 0 && !$('body').is('.extended-search-is-open')) {
+        let searchExpandBtn = $('a.topbar-link-search', context);
+        if (searchExpandBtn.length > 0 && !searchExpandBtn.is('.active')) {
           searchExpandBtn.click();
+          let body = $('body');
+
+          body.toggleClass('mobile-search-is-open');
+          body.removeClass('mobile-menu-is-open pane-login-is-open mobile-usermenu-is-open');
+          if(!body.hasClass('mobile-search-is-open')) {
+            body.addClass('overlay-is-active');
+          } else {
+            body.removeClass('overlay-is-active');
+          }
         }
       });
     }
   };
-
 })(jQuery);
