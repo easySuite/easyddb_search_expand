80445cf3e639f20fdb3213ec30a0e2cff4bc8980
Alex <bozeak@gmail.com>
2019-03-13 15:21:42 +0200

    EASYOPAC-1051 - Search expand fix for 4.6.x.

--- js/easyddb_search_expand.js
+++ js/easyddb_search_expand.js
@@ -7,12 +7,17 @@
   Drupal.behaviors.searchExpand = {
     attach: function (context) {
       $(function () {
-        let searchExpandBtn = $('a.topbar-link-search', context);
+        var searchExpandBtn = $('a.search-extended-button', context);
         if (searchExpandBtn.length > 0 && !searchExpandBtn.is('.active')) {
           searchExpandBtn.click();
-          let body = $('body');
+          var body = $('body');
+
+          if (body.hasClass('not-logged-in')) {
+            body.find('.mobile-search-is-open .site-header .navigation-wrapper', context).attr('style', 'top: 0');
+          }
 
           body.toggleClass('mobile-search-is-open');
+          body.toggleClass('extended-search-is-not-open');
           body.removeClass('mobile-menu-is-open pane-login-is-open mobile-usermenu-is-open');
           if(!body.hasClass('mobile-search-is-open')) {
             body.addClass('overlay-is-active');
