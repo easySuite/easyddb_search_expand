4f9774e23ae4b6a0b6b733ad5ecdb220c3512e89
Martin Cording <mc@inlead.dk>
2019-04-30 09:51:48 +0200

    Hotfix: Refactor module to new search changes.

--- js/easyddb_search_expand.js
+++ js/easyddb_search_expand.js
@@ -8,22 +8,18 @@
     attach: function (context) {
       $(function () {
         var searchExpandBtn = $('a.search-extended-button', context);
-        if (searchExpandBtn.length > 0 && !searchExpandBtn.is('.active')) {
-          searchExpandBtn.click();
+        if (searchExpandBtn.length > 0) {
           var body = $('body');
 
-          if (body.hasClass('not-logged-in')) {
-            body.find('.mobile-search-is-open .site-header .navigation-wrapper', context).attr('style', 'top: 0');
-          }
+          // Activate the button.
+          searchExpandBtn.click();
 
-          body.toggleClass('mobile-search-is-open');
-          body.toggleClass('extended-search-is-not-open');
-          body.removeClass('mobile-menu-is-open pane-login-is-open mobile-usermenu-is-open');
-          if(!body.hasClass('mobile-search-is-open')) {
-            body.addClass('overlay-is-active');
-          } else {
-            body.removeClass('overlay-is-active');
-          }
+          // Hide the button and remove it's class.
+          searchExpandBtn.hide();
+          body.removeClass('has-search-dropdown');
+
+          // Remove the class from body.
+          body.removeClass('extended-search-is-not-open');
         }
       });
     }
